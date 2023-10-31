78e5ca692bc05c2234fc2745c71740e06fc1253f
crazynada <149445943+crazynada@users.noreply.github.com>
2023-10-31 14:58:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,44 +4,77 @@ sky_doctor = [13, 31]
 extend_year = [19, 91]
 shadow = [11, 22]
 six_bad = [16, 61]
-damage = [17, 71]
-five_ghost = [18, 81]
-dead = [12, 21]
+disaster = [17, 71]
+five_ghosts = [18, 81]
+death = [12, 21]
 
-# Define the excluded number groups
-excluded_groups = [shadow, six_bad, damage, five_ghost, dead]
-
+# Prompt the user for the telephone number and validate the input
 while True:
-    # Prompt the user for the telephone number and validate the input
     while True:
-        tele_num = input("Enter the telephone number (10 digits): ")
+        tele_num =str(input("輸入電話號碼 (10 digits): "))
         
         if tele_num.isdigit() and len(tele_num) == 10:
             break
         else:
-            print("Invalid input. Please enter a valid 10-digit number.")
+            print("請重新輸入")
 
-    # Split the telephone number into individual groups of numbers
-    number_groups = [tele_num[i:i+2] for i in range(0, len(tele_num), 2)]
+    # Split the telephone number into individual pairs of digits
+    number_groups = [tele_num[i:i+2] for i in range(len(tele_num) - 1)]
+    number_groups.append(tele_num[-2:])
 
     # Initialize variables to track the positions of the groups
     position_anger = None
     position_sky_doctor = None
     position_extend_year = None
+    position_shadow = None
+    position_six_bad = None
+    position_disaster = None
+    position_five_ghosts = None
+    position_death = None
 
-    # Check each group against the defined number groups
+    # Initialize a set to keep track of matched groups
+    matched_groups = set()
+    
+    # Check each pair against the defined number groups
     for index, group in enumerate(number_groups):
+        if group in matched_groups:
+            continue  # Skip this group if it's already matched
+    
         if group in [str(num) for num in anger]:
-            print(f"Group {group} is in anger.")
+            print(f"{group} matched 生氣.")
             position_anger = index
-        elif group in [str(num) for num in sky_doctor]:
-            print(f"Group {group} is in sky_doctor.")
+            matched_groups.add(group)  # Mark the group as matched
+        if group in [str(num) for num in sky_doctor]:
+            print(f"{group} matched 天醫.")
             position_sky_doctor = index
-        elif group in [str(num) for num in extend_year]:
-            print(f"Group {group} is in extend_year.")
+            matched_groups.add(group)
+        if group in [str(num) for num in extend_year]:
+            print(f"{group} matched 延年.")
             position_extend_year = index
-        elif "0" in group and index == len(number_groups) - 1:
-            print(f"Group {group} contains '0' and is at the end of tele_num.")
+            matched_groups.add(group)
+        if group in [str(num) for num in shadow]:
+            print(f"{group} matched 伏位.")
+            position_shadow = index
+            matched_groups.add(group)
+        if group in [str(num) for num in six_bad]:
+            print(f"{group} matched 六煞.")
+            position_six_bad = index
+            matched_groups.add(group)
+        # if group in [str(num) for num in disaster]:
+        #     print(f"{group} matched 禍害.")
+        #     position_disaster = index
+        #     matched_groups.add(group)
+        # if group in [str(num) for num in five_ghosts]:
+        #     print(f"{group} matched 五鬼.")
+        #     position_five_ghosts = index
+        #     matched_groups.add(group)
+        # if group in [str(num) for num in death]:
+        #     print(f"{group} matched 絕命.")
+        #     position_death = index
+        #     matched_groups.add(group)
+        if "0" in group and index == len(number_groups) - 1:
+            print(f"{group} 出現字尾")
+
 
     # Check the order of the groups
     if (
@@ -51,11 +84,11 @@ while True:
         and position_extend_year is not None
         and position_extend_year > position_sky_doctor
     ):
-        print("Order of groups: anger is superior to sky_doctor, and sky_doctor is superior to extend_year.")
+        print("黃金組合: 生氣 > 天醫 > 延年")
 
-    # Ask the user if they want to check another number
-    another = input("Do you want to check another number? (yes/no): ")
-    if another.lower() not in ('yes', 'y'):
-        break
+    # # Ask the user if they want to check another number
+    # another = input("Do you want to check another number? (yes/no): ")
+    # if another.lower() not in ('yes', 'y'):
+    #     break
 
-print("End of the program.")
+# print("End of the program.")
